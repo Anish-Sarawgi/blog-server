06d4c76741f7cc60cac8962ed981a74136e49238
Anish Sarawgi <anishsarawgi6@gmail.com>
2022-12-23 14:33:17 +0530

    refactor: add environmental variable

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const articleRoutes = require('./api/articleRoutes');
-const authRoutes = require('./api/authRoutes');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const articleRoutes = require("./api/articleRoutes");
+const authRoutes = require("./api/authRoutes");
 // const userRoutes = require('./api/userRoutes.js');
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const port = process.env.PORT ||7000;
+const port = process.env.PORT || 7000;
 
-const dbURI = 'mongodb+srv://freeRabbit:test1234@cluster0.o5l7t.mongodb.net/MM_News?retryWrites=true&w=majority'
+const dbURI = process.env.MONGO_URI;
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(port)) 
-  .catch(err => console.log(err));
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err));
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
 // app.use('/api/user', userRoutes);
-app.use('/api/article', articleRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/article", articleRoutes);
+app.use("/api/auth", authRoutes);
